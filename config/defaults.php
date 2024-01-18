41551913900f4120f9cbf08bf679017d68b16a39
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-18 19:32:56 +0200

    Create defaults.php

---
+++ config/defaults.php
@@ -0,0 +1,27 @@
+<?php
+
+// Application default settings
+
+// Error reporting
+error_reporting(0);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
+// Timezone
+date_default_timezone_set('Europe/Vilnius');
+
+$settings = [];
+
+// Error handler
+$settings['error'] = [
+    // Should be set to false for the production environment
+    'display_error_details' => false,
+];
+
+// Logger settings
+$settings['logger'] = [
+    // Default log level
+    'level' => \Psr\Log\LogLevel::INFO,
+];
+
+return $settings;
