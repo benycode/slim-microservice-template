3bcd382e71396abd31df30fc4b96007c0b1cb560
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-19 11:39:25 +0200

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,16 +1,16 @@
 <?php
 
-use DI\ContainerBuilder;
 use Slim\App;
+use DI\ContainerBuilder;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Build DI container instance
-$container = (new ContainerBuilder())
+$container = (new ContainerBuilder)
     ->addDefinitions(__DIR__ . '/container.php')
     ->build();
 
 // Create App instance
 return $container
-	->get(App::class)
-	;
+    ->get(App::class)
+;
