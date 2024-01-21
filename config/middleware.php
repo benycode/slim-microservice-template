c01cdafa8a6af440f0af6277420b8c3237b49004
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-21 12:15:07 +0200

    Update middleware.php

--- config/middleware.php
+++ config/middleware.php
@@ -3,6 +3,7 @@
 use Slim\App;
 use Selective\BasePath\BasePathMiddleware;
 use BenyCode\Slim\Middleware\SettingsUpMiddleware;
+use BenyCode\Slim\Middleware\ExceptionMiddleware;
 
 return function (App $app) {
     $app->addBodyParsingMiddleware();
