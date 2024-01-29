beb001a13bddcada300efb46c8a855a754df3187
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-29 20:51:42 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,24 @@
 <?php
 
-(require_once __DIR__ . '/../config/bootstrap.php')
-	->run()
-	;
+use Slim\App;
+use DI\ContainerBuilder;
+use BenyCode\Slim\Abstraction\DI;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = (new ContainerBuilder)
+    ->addDefinitions(
+        \array_merge(
+            DI::create(__DIR__ . '/../config'),
+            [
+             //
+            ],
+        ),
+    )
+    ->build()
+;
+
+$container
+    ->get(App::class)
+    ->run()
+;
