91c7de2d553ef41192418c20930268d5d86ca358
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-30 17:21:32 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,24 +1,26 @@
 <?php
 
-use Slim\App;
-use DI\ContainerBuilder;
-use BenyCode\Slim\Abstraction\DI;
+error_reporting(0);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
+if (isset($_ENV['api_error']) && is_string($_ENV['api_error']) && null !== json_decode($_ENV['api_error'])) {
+    $api_error = json_decode($_ENV['api_error'], true);
+
+    if(true === $api_error['display_error_details']) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+    }
+}
+
+use BenyCode\Slim\Abstraction\App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = (new ContainerBuilder)
-    ->addDefinitions(
-        \array_merge(
-            DI::create(__DIR__ . '/../config'),
-            [
-             //
-            ],
-        ),
-    )
-    ->build()
-;
+date_default_timezone_set('Europe/Vilnius');
 
-$container
-    ->get(App::class)
-    ->run()
-;
+App::run(
+    [__DIR__ . '/../app/Action/'],
+    [],
+);
